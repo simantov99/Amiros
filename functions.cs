4f9cc6ed65b6d76c733242ce643d48db2273342a
simantov99 <12yair3@gmail.com>
2017-03-16 14:59:33 +0200

    Update functions.cs

--- functions.cs
+++ functions.cs
@@ -1,1 +1,30 @@
-//edit this
+class Functions
+    {
+        
+
+        public Drone ClosesEnemytDroneToMApObject(PirateGame game, MapObject L)
+        {
+            try
+            {
+                return game.GetEnemyLivingDrones().OrderBy(Drone => Drone.Distance(L)).ToList()[0];
+            }
+            catch (Exception)
+            {
+
+                return null;
+            }
+        }
+        public List<Aircraft> AirCraftInRange(PirateGame game , Aircraft Ac , int Radius)
+        {
+            try
+            {
+                return game.GetEnemyLivingAircrafts().Where(Aircraft => Ac.InRange(Aircraft, Radius)).ToList();
+            }
+            catch (Exception)
+            {
+
+                return null;
+            }
+        }
+        
+    }
