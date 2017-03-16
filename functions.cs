8aad5ee9bcadd0615d6929e6c50f2dbd3ecaa277
simantov99 <12yair3@gmail.com>
2017-03-16 14:31:37 +0200

    Update functions.cs
    SendAirCraftToLocation() => שולח אירקאפט למיקום
    ClosestEnemyDroneToMapObject() => האיירקראפט הכי קרוב לאובייקט מפה

--- functions.cs
+++ functions.cs
@@ -1,1 +1,21 @@
-//edit this
+class Functions
+    {
+        
+        public static void SendAirCraftToLocation(PirateGame game , Aircraft Ac , Location L)
+        {
+            List<Location> _sailOptions = game.GetSailOptions(Ac,L);
+            game.SetSail(Ac,_sailOptions[0]);
+        }
+        public Drone ClosesEnemytDroneToMapObject(PirateGame game, MapObject L)
+        {
+            try
+            {
+                return game.GetEnemyLivingDrones().OrderBy(Drone => Drone.Distance(L)).ToList()[0];
+            }
+            catch (Exception)
+            {
+
+                return null;
+            }
+        }
+    }
